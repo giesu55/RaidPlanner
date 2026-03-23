eaf22b42e1ae706b24fcdadcfae09a0864110f2d
gabriel <gabriel.suchocki@infotech.edu.pl>
2026-03-23 12:34:56 +0100

    Dodano funkcjonalność do historii

---
+++ HistoryEntry.cs
@@ -0,0 +1,13 @@
+namespace RaidPlanner
+{
+    public class HistoryEntry
+    {
+        public string MapName { get; set; }
+
+        public string WishlistName { get; set; }
+
+        public string FoundItems { get; set; }
+
+        public string Date { get; set; }
+    }
+}
